3c4576e14a43abcfca3419e7a71caae70d9df12c
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2023-03-25 21:54:56 +0700

    fix: change input type search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -10,7 +10,7 @@ export const Search = ({ searchQuery }) => {
     <div className="searchbar-container">
       <form>
         <input
-          type="text"
+          type="search"
           placeholder={placeholder}
           className="searchInput"
           value={query}
